a1d8d1c0d7a65807ddae8e148a19f22bb80ff0b1
Mickey DeLorenzo <mdelorenzo@byrdalley.com>
2022-02-08 10:31:08 -0500

    Added Slick Carousel

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Carousel from "../components/carousel"
 
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Carousel></Carousel>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
